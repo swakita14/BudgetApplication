3de8ef0fca5074ce4e1efacd6f3e05d1a98b5842
Shion Wakita <swakita14@wou.edu>
2019-08-23 19:20:53 +0900

    Added item model and dbcontext

---
+++ BudgetApplication/Models/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BudgetApplication.Models
+{
+    public class Item
+    {
+
+        public int ItemID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Price { get; set; }
+
+        public int CategoryID { get; set; }
+
+    }
+
+}
