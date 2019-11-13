0c45c684fcba3ba8e67df86fc3990155fc67190d
Shion Wakita <swakita14@wou.edu>
2019-11-12 18:00:33 -0800

    integrated datepicker into application

--- BudgetApplication/wwwroot/js/chartView.js
+++ BudgetApplication/wwwroot/js/chartView.js
@@ -1,4 +1,14 @@
 ﻿$(document).ready(function () {
     console.log("ready!");
     $('#myTable').DataTable();
+
+    $("#modal-btn").on("click", function () {
+        console.log('this is it folks!');
+        $(function () {
+            $('[data-toggle="datepicker"]').datepicker({
+                autoHide: true,
+                zIndex: 2048,
+            });
+        });
+    });
 });
