d157bf90f13bd6e6e0018e4dbab1984368bee570
Shion Wakita <swakita14@wou.edu>
2019-11-13 17:31:41 -0800

    adding attributes to model

--- BudgetApplication/Models/ViewModels/AddItemViewModel.cs
+++ BudgetApplication/Models/ViewModels/AddItemViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Security.Permissions;
 using System.Threading.Tasks;
@@ -10,12 +11,22 @@ namespace BudgetApplication.Models.ViewModels
     {
         public int ItemId { get; set; }
 
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Item Name")]
         public string Name { get; set; }
 
+        [Required]
+        [Display(Name = "Price")]
         public decimal Price { get; set; }
 
+        [Required]
+        [Display(Name = "Category")]
         public string Category { get; set; }
 
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Purchased Date")]
         public string PurchaseDate { get; set; }
 
     }
