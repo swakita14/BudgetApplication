c83907719646b395b9903d85e596ee2cd36184dc
Shion Wakita <swakita14@wou.edu>
2019-11-16 18:00:18 -0800

    added new controller for a spending per month view

---
+++ BudgetApplication/Controllers/SpendingAnalysisController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BudgetApplication.Interface.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetApplication.Controllers
+{
+    public class SpendingAnalysisController : Controller
+    {
+        private readonly IItemRepository _itemRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public SpendingAnalysisController(IItemRepository itemRepository, ICategoryRepository categoryRepository)
+        {
+            _itemRepository = itemRepository;
+            _categoryRepository = categoryRepository;
+        }
+    }
+}
